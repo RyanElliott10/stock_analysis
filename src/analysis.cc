b53d9b1ff1ca9e3697e648f6a93db85bd0bed430
Ryan Elliott <ryan.elliott31@gmail.com>
2018-10-25 23:30:12 -0700

    removed comments

--- src/analysis.cc
+++ src/analysis.cc
@@ -1,17 +1,3 @@
-/**
- * MOVE THIS TO THE README.MD EVENTUALLY
- * 
- * Coding standard
- * 
- * 2 spaces
- * files are to be named_like_this.cc (NOT .cpp)
- * StrongCamelCase for classes, structs, enums, etc. (all types)
- * underscore_vars for vars and public functions
- * _underscore_funcs for private functions
- * struct_data_members follow normal variable naming conventions
- * var_name_ for class members
- */
-
 #include <iostream>
 #include <string>
 
